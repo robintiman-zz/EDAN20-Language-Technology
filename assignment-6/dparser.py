3c6e3733cc8c43ecf4492980d7e2ed7b9abad36c
Robin Timan <robintiman@gmail.com>
2017-10-12 11:31:06 +0200

    ..

--- assignment-6/dparser.py
+++ assignment-6/dparser.py
@@ -3,17 +3,14 @@ Gold standard parser
 """
 __author__ = "Pierre Nugues"
 
-
 from sklearn import preprocessing
 from sklearn.feature_extraction import DictVectorizer
 from sklearn import linear_model
 from sklearn import metrics
-
+import pickle
 import features
 import transition
 import conll
-
-
 
 def reference(stack, queue, graph):
     """
@@ -70,21 +67,26 @@ def parse_ml(stack, queue, graph, trans):
     return stack, queue, graph, 'sh'
 
 
-
-def calc(file, model=None, le=None, vec=None):
+def parse(file, classifier, le=None, vec=None):
     column_names_2006 = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
     column_names_2006_test = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats']
 
-    feature_names = [("stack", 0, "postag"), ("stack", 1, "postag"), ("stack", 0, "form"), ("stack", 1, "form"),
-                     ("queue", 0, "postag"), ("queue", 1, "postag"), ("queue", 0, "form"), ("queue", 1, "form")]
-    # "can_re", "can_la"]
+    feature_names = [("stack", 0, "postag", "stack0_POS"), ("queue", 0, "postag", "queue0_POS"),
+                     ("stack", 0, "form", "stack0_word"), ("queue", 0, "form", "queue0_word"),
+                     ("stack", 1, "postag", "stack1_POS"), ("stack", 1, "form", "stack1_word"),
+                     ("queue", 1, "postag", "queue1_POS"), ("queue", 1, "form", "queue1_word"),
+                     # Here the index is in id or head
+                     ("sentence", ("stack", 0, "id"), "postag", "stack0_fw_POS"),
+                     ("sentence", ("stack", 0, "id"), "form", "stack0_fw_word"),
+                     ("sentence", ("stack", 1, "head"), "postag", "stack0_head_POS"),
+                     ("sentence", ("stack", 1, "head"), "form", "stack0_head_word")]
 
     sentences = conll.read_sentences(train_file)
     formatted_corpus = conll.split_rows(sentences, column_names_2006)
 
     sent_cnt = 0
-    X_dict = []
-    y_dict = []
+    X = []
+    y = []
     for sentence in formatted_corpus:
         sent_cnt += 1
         if sent_cnt % 1000 == 0:
@@ -96,22 +98,20 @@ def calc(file, model=None, le=None, vec=None):
         graph['heads']['0'] = '0'
         graph['deprels'] = {}
         graph['deprels']['0'] = 'ROOT'
-        transitions = []
         while queue:
             feat = features.extract(stack, queue, graph, feature_names, sentence)
+            trans_nr = classifier.predict(feat)
+            stack, queue, graph, trans = parse_ml(stack, queue, graph, trans_nr)
 
-            if model is None or vec is None or le is None:
-                stack, queue, state, trans = reference(stack, queue, state)
-                transitions.append(trans)
-            else:
-                encode_feat = vec.transform(feat)
-                trans_nr = classifier.predict(encode_feat)
-                trans = le.inverse_transform(trans_nr)
-                stack, queue, graph, trans = parse_ml(stack, queue, graph, trans)
-                transitions.append(trans)
-
-            X_dict.append(feat)
-            y_dict.append(trans)
+            # if model is None or vec is None or le is None:
+            #     stack, queue, state, trans = reference(stack, queue, state)
+            #     transitions.append(trans)
+            # else:
+            #     encode_feat = vec.transform(feat)
+            #     trans_nr = classifier.predict(encode_feat)
+            #     trans = le.inverse_transform(trans_nr)
+            #     stack, queue, graph, trans = parse_ml(stack, queue, graph, trans)
+            #     transitions.append(trans)
 
         stack, graph = transition.empty_stack(stack, graph)
         print('Equal graphs:', transition.equal_graphs(sentence, graph))
@@ -121,31 +121,38 @@ def calc(file, model=None, le=None, vec=None):
             word['head'] = graph['heads'][word['id']]
        # print(transitions)
        # print(graph)
-    return X_dict, y_dict
+    return X, y
 
 
 if __name__ == '__main__':
-    train_file = 'swedish_talbanken05_train.conll'
-    test_file = 'swedish_talbanken05_test_blind.conll'
-    X_train, y_train = calc(train_file)
-    X_test, y_test = calc(test_file)
+
+    feature_lengths = [4]
+    for num_features in feature_lengths:
+        train_file = 'swedish_talbanken05_train.conll'
+        test_file = 'swedish_talbanken05_test_blind.conll'
+
+        with open("model" + str(num_features), "rb") as f:
+            classifier = pickle.load(f)
+
+        X_train, y_train = parse(train_file, classifier)
+        X_test, y_test = parse(test_file, classifier)
 
 
     # Vectorize the feature matrix and carry out a one-hot encoding
-    le = preprocessing.LabelEncoder()
-    y = le.fit_transform(y_train)
-    vec = DictVectorizer(sparse=True)
-    X = vec.fit_transform(X_train)
-
-    print("Training the model...")
-    classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
-    model = classifier.fit(X, y)
-    print("predicting...")
-    y_pred = model.predict(X)
-    accuracy = metrics.accuracy_score(y, y_pred)
-    print("Accuracy on train {0}".format(accuracy))
-    X_test = le.fit_transform(X_train)
-    y_test = vec.fit_transform(X_train)
-    y_pred = model.predict(X_test)
-    accuracy = metrics.accuracy_score(y_test,y_pred)
-    print("Accuracy on test {0}".format(accuracy))
+    # le = preprocessing.LabelEncoder()
+    # y = le.fit_transform(y_train)
+    # vec = DictVectorizer(sparse=True)
+    # X = vec.fit_transform(X_train)
+    #
+    # print("Training the model...")
+    # classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
+    # model = classifier.fit(X, y)
+    # print("predicting...")
+    # y_pred = model.predict(X)
+    # accuracy = metrics.accuracy_score(y, y_pred)
+    # print("Accuracy on train {0}".format(accuracy))
+    # X_test = le.fit_transform(X_train)
+    # y_test = vec.fit_transform(X_train)
+    # y_pred = model.predict(X_test)
+    # accuracy = metrics.accuracy_score(y_test,y_pred)
+    # print("Accuracy on test {0}".format(accuracy))
