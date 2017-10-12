dec3f46a79f02194253c7133e2e01fcbc8e6d851
Robin Timan <robintiman@gmail.com>
2017-10-12 09:44:03 +0200

    Now doing three models in 5

--- assignment-5/dparser.py
+++ assignment-5/dparser.py
@@ -12,6 +12,7 @@ from sklearn import metrics
 import features
 import transition
 import conll
+import pickle
 
 
 
@@ -51,20 +52,29 @@ def reference(stack, queue, graph):
     stack, queue, graph = transition.shift(stack, queue, graph)
     return stack, queue, graph, 'sh'
 
-def calc(file):
+def parse(file, save_as, num_features):
     column_names_2006 = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
     column_names_2006_test = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats']
 
-    feature_names = [("stack", 0, "postag"), ("stack", 1, "postag"), ("stack", 0, "form"), ("stack", 1, "form"),
-                     ("queue", 0, "postag"), ("queue", 1, "postag"), ("queue", 0, "form"), ("queue", 1, "form")]
-    # "can_re", "can_la"]
+    # can-re and can-la are added manually in extract()
+    feature_names = [("stack", 0, "postag", "stack0_POS"), ("queue", 0, "postag", "queue0_POS"),
+                     ("stack", 0, "form", "stack0_word"), ("queue", 0, "form", "queue0_word"),
+                     ("stack", 1, "postag", "stack1_POS"), ("stack", 1, "form", "stack1_word"),
+                     ("queue", 1, "postag", "queue1_POS"), ("queue", 1, "form", "queue1_word"),
+                     # Here the index is in id or head
+                     ("sentence", ("stack", 0, "id"), "postag", "stack0_fw_POS"),
+                     ("sentence", ("stack", 0, "id"), "form", "stack0_fw_word"),
+                     ("sentence", ("stack", 1, "head"), "postag", "stack0_head_POS"),
+                     ("sentence", ("stack", 1, "head"), "form", "stack0_head_word")]
+
+    feature_names = feature_names[:num_features]
 
     sentences = conll.read_sentences(train_file)
     formatted_corpus = conll.split_rows(sentences, column_names_2006)
 
     sent_cnt = 0
-    X_dict = []
-    y_dict = []
+    X = []
+    y = []
     for sentence in formatted_corpus:
         sent_cnt += 1
         if sent_cnt % 1000 == 0:
@@ -76,48 +86,44 @@ def calc(file):
         graph['heads']['0'] = '0'
         graph['deprels'] = {}
         graph['deprels']['0'] = 'ROOT'
-        transitions = []
         while queue:
             feat = features.extract(stack, queue, graph, feature_names, sentence)
             stack, queue, graph, trans = reference(stack, queue, graph)
-            transitions.append(trans)
+            X.append(feat)
+            y.append(trans)
 
-            X_dict.append(feat)
-            y_dict.append(trans)
+    with open("X_{}.data".format(save_as), "wb") as f:
+        pickle.dump(X, f)
 
-        stack, graph = transition.empty_stack(stack, graph)
-        print('Equal graphs:', transition.equal_graphs(sentence, graph))
+    with open("y_{}.data".format(save_as), "wb") as f:
+        pickle.dump(y, f)
 
-        # Poorman's projectivization to have well-formed graphs.
-        for word in sentence:
-            word['head'] = graph['heads'][word['id']]
-       # print(transitions)
-       # print(graph)
-    return X_dict, y_dict
+    return X, y
 
 
 if __name__ == '__main__':
     train_file = 'swedish_talbanken05_train.conll'
-    test_file = 'swedish_talbanken05_test_blind.conll'
-    X_train, y_train = calc(train_file)
-    X_test, y_test = calc(test_file)
+    test_file = 'swedish_talbanken05_test.conll'
 
+    feature_lengths = [4, 8, 12]
+    for num_features in feature_lengths:
+        X_train, y_train = parse(train_file, "train_" + str(num_features), num_features)
+        X_test, y_test = parse(test_file, "test_" + str(num_features), num_features)
+        # Vectorize the feature matrix and carry out a one-hot encoding
+        le = preprocessing.LabelEncoder()
+        y = le.fit_transform(y_train)
+        vec = DictVectorizer(sparse=True)
+        X = vec.fit_transform(X_train)
 
-    # Vectorize the feature matrix and carry out a one-hot encoding
-    le = preprocessing.LabelEncoder()
-    y = le.fit_transform(y_train)
-    vec = DictVectorizer(sparse=True)
-    X = vec.fit_transform(X_train)
-
-    print("Training the model...")
-    classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
-    model = classifier.fit(X, y)
-    print("predicting...")
-    y_pred = model.predict(X)
-    accuracy = metrics.accuracy_score(y, y_pred)
-    print("Accuracy on train {0}".format(accuracy))
-    X_test = le.fit_transform(X_train)
-    y_test = vec.fit_transform(X_train)
-    y_pred = model.predict(X_test)
-    accuracy = metrics.accuracy_score(y_test,y_pred)
-    print("Accuracy on test {0}".format(accuracy))
+        print("Training the model...")
+        classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
+        model = classifier.fit(X, y)
+        print("predicting...")
+        y_pred = model.predict(X)
+        accuracy = metrics.accuracy_score(y, y_pred)
+        print("Accuracy on train {0}".format(accuracy))
+        X_test = le.fit_transform(X_train)
+        y_test = vec.fit_transform(X_train)
+        y_pred = model.predict(X_test)
+        accuracy = metrics.accuracy_score(y_test,y_pred)
+        print("Accuracy on test {0}".format(accuracy))
